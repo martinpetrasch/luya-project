7917c513b86e09b16f0b49d8971c1716ed3ea6c6
nadar <basil.suter@zephir.ch>
2015-01-22 13:05:05 +0100

    added test module actions and urlRules

--- modules/estore/Module.php
+++ modules/estore/Module.php
@@ -3,4 +3,9 @@ namespace app\modules\estore;
 
 class Module extends \luya\base\Module
 {
+    public static $urlRules = [
+        ['pattern' => 'estore/testof/<id:\d+>', 'route' => 'estore/default/debug'],
+        ['pattern' => 'estore/xyz', 'route' => 'estore/default/debug'],
+    ];
+    
 }
--- modules/estore/controllers/DefaultController.php
+++ modules/estore/controllers/DefaultController.php
@@ -7,15 +7,11 @@ class DefaultController extends \luya\base\Controller
 {
     public function actionIndex()
     {
-        $links = new \estore\collection\Links();
-        $links->setLink('ja', ['yuhu']);
-        Yii::$app->get('collection')->links = $links;
-        $page = new \luya\collection\Page();
-        $page->title = 'e_store';
-        $page->content = 'e_store content';
-
-        Yii::$app->get('collection')->page = $page;
-
-        return $this->render("index", ['page' => $page]);
+        return 'ICH BIN DIE ACTION VON ESTORE DEFAULT';
+    }
+    
+    public function actionDebug()
+    {
+        return 'ICB BIN DEBUG CONTEXT!';
     }
 }

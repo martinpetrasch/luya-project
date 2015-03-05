c5a561f9b127ebdfcc33e3d0c16046cd214a88b1
nadar <git@nadar.io>
2015-03-05 15:33:03 +0100

    new luya base boot implementation

--- public_html/index.php
+++ public_html/index.php
@@ -1,19 +1,10 @@
 <?php
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+
 // composer & yii include
 require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+
 // use the luya boot wrapping class
-use \luya\base\Boot;
-// load your local customer repo config
-$config = include('../config/local.php');
-// start the boot class
-$boot = new Boot();
-// set configuration values for WEB mode
-$boot->setConfig(Boot::SAPI_WEB, $config);
-// set configuration values for CONSOLE/CLI mode
-$boot->setConfig(Boot::SAPI_CLI, $config);
-// run the Yii application (the wrapping class decides between WEB or CLI)
+$boot = new \luya\base\Boot();
+$boot->setYiiPath(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 $boot->run();
